833c8925a1593881d4e15f82a5ab2aee73c3a5de
Cecret <Cecret3350@outlook.com>
2022-02-25 20:30:24 +0800

    complete project 0!

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -1,5 +1,4 @@
 public class Planet {
-
     public double xxPos;
     public double yyPos;
     public double xxVel;
@@ -7,7 +6,7 @@ public class Planet {
     public double mass;
     public String imgFileName;
 
-    static final double G = 6.67e-11;
+    private final double G = 6.67e-11;
 
     public Planet(double xP, double yP, double xV,
                   double yV, double m, String img) {
