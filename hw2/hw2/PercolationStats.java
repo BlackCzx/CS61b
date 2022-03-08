8f3736869de055be3b9a796dbc3cb7d95493d187
Cecret <Cecret3350@outlook.com>
2022-03-08 10:32:18 +0800

    complete HW 2

--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,55 @@
 package hw2;
 
+import edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.introcs.StdStats;
+
 public class PercolationStats {
+
+    private int gridSize;
+    private int iterations;
+    private Percolation p;
+
+    private double[] thresholds;
+
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0 || T <= 0) {
+            throw new java.lang.IllegalArgumentException();
+        }
+        gridSize = N;
+        iterations = T;
+        p = pf.make(gridSize);
+        thresholds = new double[iterations];
+
+        for (int i = 0; i < iterations; i++) {
+            thresholds[i] = experiment();
+        }
+    }
+
+    private double experiment() {
+        int row;
+        int col;
+        while (!p.percolates()) {
+            row = StdRandom.uniform(gridSize);
+            col = StdRandom.uniform(gridSize);
+            p.open(row, col);
+        }
+        return p.numberOfOpenSites() / (gridSize * gridSize);
+    }
+
+    public double mean() {
+        return StdStats.mean(thresholds);
+    }
+
+    public double stddev() {
+        return StdStats.stddev(thresholds);
+    }
+
+    public double confidenceLow() {
+        return mean() - 1.96 * stddev() / Math.sqrt(iterations);
+    }
+
+    public double confidenceHigh() {
+        return mean() + 1.96 * stddev() / Math.sqrt(iterations);
+    }
 
 }
