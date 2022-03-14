5e4b856d9b6d85a42672c896a65a82833a0cd3fd
Cecret <Cecret3350@outlook.com>
2022-03-14 15:28:49 +0800

    complete Lab 11 with detecting circle

--- lab11/lab11/graphs/MazeBreadthFirstPaths.java
+++ lab11/lab11/graphs/MazeBreadthFirstPaths.java
@@ -1,4 +1,6 @@
 package lab11.graphs;
+import java.util.LinkedList;
+import java.util.Queue;
 
 /**
  *  @author Josh Hug
@@ -10,20 +12,49 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
     public boolean[] marked;
     */
 
+    private int s;
+    private int t;
+    private Queue<Integer> q;
+
     public MazeBreadthFirstPaths(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
+        s = m.xyTo1D(sourceX, sourceY);
+        t = m.xyTo1D(targetX, targetY);
+        distTo[s] = 0;
+        edgeTo[s] = s;
+
+        q = new LinkedList<>();
+        q.add(s);
         // Add more variables here!
     }
 
     /** Conducts a breadth first search of the maze starting at the source. */
     private void bfs() {
-        // TODO: Your code here. Don't forget to update distTo, edgeTo, and marked, as well as call announce()
+        int v;
+        while (!q.isEmpty()) {
+            v = q.remove();
+            marked[v] = true;
+            announce();
+
+            if (v == t) {
+                return;
+            }
+
+            for (int w : maze.adj(v)) {
+                if (!marked[w]) {
+                    distTo[w] = distTo[v] + 1;
+                    edgeTo[w] = v;
+                    announce();
+                    q.add(w);
+                }
+            }
+        }
     }
 
 
     @Override
     public void solve() {
-        // bfs();
+        bfs();
     }
 }
 
--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -10,13 +10,65 @@ public class MazeCycles extends MazeExplorer {
     public boolean[] marked;
     */
 
+    private int unMarkedV;
+    private boolean markAllFlag;
+    private boolean findCircleFlag;
+    private boolean finishDrawCircle;
+    private int circleV;
+
     public MazeCycles(Maze m) {
         super(m);
+        //distTo[0] = 0;
+        edgeTo[0] = 0;
+        unMarkedV = m.V();
+        markAllFlag = false;
+        findCircleFlag = false;
+        finishDrawCircle = false;
+    }
+
+    private void dfs(int v, int parent) {
+        marked[v] = true;
+        announce();
+        unMarkedV--;
+        if (unMarkedV == 0) {
+            markAllFlag = true;
+        }
+        if (markAllFlag) {
+            return;
+        }
+        for (int w : maze.adj(v)) {
+            if (w != parent) {
+                if (marked[w]) {
+                    findCircleFlag = true;
+                    circleV = w;
+                } else {
+                    dfs(w, v);
+                }
+            }
+            if (markAllFlag) {
+                return;
+            }
+            if (findCircleFlag) {
+                if (v == circleV) {
+                    finishDrawCircle = true;
+                    edgeTo[w] = v;
+                    announce();
+                    return;
+                }
+                if (finishDrawCircle) {
+                    return;
+                } else {
+                    edgeTo[w] = v;
+                    announce();
+                    return;
+                }
+            }
+        }
     }
 
     @Override
     public void solve() {
-        // TODO: Your code here!
+        dfs(0, 0);
     }
 
     // Helper methods go here
