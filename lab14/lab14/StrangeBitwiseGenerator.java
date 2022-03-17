94774d3787da1c220f126003a2dab3bd7b7f19e5
Cecret <Cecret3350@outlook.com>
2022-03-18 00:12:18 +0800

    complete Lab 14

--- lab14/lab14/StrangeBitwiseGenerator.java
+++ lab14/lab14/StrangeBitwiseGenerator.java
@@ -1,4 +1,21 @@
 package lab14;
 
-public class StrangeBitwiseGenerator {
+import lab14lib.Generator;
+
+public class StrangeBitwiseGenerator implements Generator {
+    private int frequency;
+    private int state;
+
+    public StrangeBitwiseGenerator(int frequency) {
+        state = 0;
+        this.frequency = frequency;
+    }
+
+    @Override
+    public double next() {
+        state = state + 1;
+        //int weirdState = state & (state >>> 3) % frequency;
+        int weirdState = state & (state >> 3) & (state >> 8) % frequency;
+        return (-1.0 + ((double) weirdState / (double) frequency) * 2.0);
+    }
 }
